eaba8994699a6f211a41b18e28cf5444a9ec1a0d
Saptarshi Mondal <mondalsaptarshi7@gmail.com>
2022-06-03 14:46:40 +0530

    Open image in new tab implemented

--- src/popup/js/views/ImagesView.js
+++ src/popup/js/views/ImagesView.js
@@ -18,7 +18,7 @@ class ImagesView extends View {
 							<div data-id="${i}" id="card_${i}" class="card ${ img.checked ? 'checked' : '' }" style="min-height: 200px;"><img src="${img.src}" style="min-width: 50px; max-width: 200px;">
                 <div class="checkbox"></div>
                 <div class="actions">
-                  <button type="button" title="Open in new tab" style="background-image: url(${openIcon});"></button>
+                  <button type="button" data-url="${img.src}" class="open_image" title="Open in new tab" style="background-image: url(${openIcon});"></button>
                   <button type="button" title="Download" style="background-image: url(${downloadIcon});"></button>
                 </div>
                 <div class="image_url_container"><input type="text" readonly='true' value="${img.src}"></div>
@@ -55,6 +55,17 @@ class ImagesView extends View {
 			}
 		});
 
+		this._addHandlerOpenSingleImage();
+
+	}
+
+	_addHandlerOpenSingleImage(){
+		this._parent.addEventListener('click', function(e){
+			if(e.target.classList.contains('open_image')){
+				window.open(e.target.dataset.url, '_blank');
+				window.close()
+			}
+		})
 	}
 
 }
